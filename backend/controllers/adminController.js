4579d00b1d64829b4bef94c6f92f58fee9c625f9
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-06-05 15:14:42 +0300

    Get All Posts and Getting post with type functionalities has been added

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -17,3 +17,22 @@ exports.createPost = async (req, res) => {
     res.status(500).json({ error: "Server error." });
   }
 };
+
+exports.getPosts= async(req,res)=>{
+  try {
+    const allPosts = await postModel.find().sort({ createdAt: -1 }); // oluşturulma tarihine göre tersten sıralar
+    res.status(200).json(allPosts);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+}
+
+exports.getPostWithType = async (req,res) => {
+  try {
+    const type = req.body.postType;
+    const post = await postModel.find({postType:type});
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({message:error.message});
+  }
+}
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../config/multerConfig');
-const { uploadFile, createPost } = require('../controllers/adminController');
+const { uploadFile, createPost, getPosts, getPostWithType } = require('../controllers/adminController');
 
 router.post('/admin/upload', upload.single('file'), uploadFile);
 
 router.post('/admin/addpost', createPost);
 
+router.get('/admin/yazilar',getPosts);
+
+router.post('/admin/yazilars',getPostWithType);
+
 router.get('/admin', (req, res) => {
   res.send('Admin paneline hoşgeldiniz.');
 });
+
+
 
 module.exports = router;

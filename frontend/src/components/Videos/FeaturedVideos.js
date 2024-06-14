30bbdc4a2aa12b644ed092fccdc3945ccbe86955
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-06-14 18:48:29 +0300

    s string deleted

--- frontend/src/components/Videos/FeaturedVideos.js
+++ frontend/src/components/Videos/FeaturedVideos.js
@@ -58,7 +58,7 @@ const FeaturedVideos = () => {
       </div>
       <div className="flex justify-center text-gray-900 dark:text-white">
         <Link to="/videolar/listele" className="hover:text-emerald-600 border-b border-transparent hover:border-emerald-600 pb-1 transition-colors duration-300 ease-in-out">Tüm Videolar</Link>
-      </div>s
+      </div>
     </div>
   );
 };

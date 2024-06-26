3c646d450bb0dfdae6c97edf239bb0cfdf2abd39
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-06-26 17:18:30 +0300

    new logo added

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -323,7 +323,7 @@ const Header = () => {
           title="teslimolan anasayfaya gidin"
           className="flex items-center"
         >
-          <img src="/logo03.jpg" className="h-12 md:h-20" alt="logo" />
+          <img src="/teslimiyet-logo.png" className="h-12 md:h-20" alt="logo" />
           <span className="text-gray-900 dark:text-white ml-3">
             Mutluluk Tanrıya Teslimiyettir
           </span>

378e2902a228423d1654168ba81676f9db8ca158
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-06-27 16:10:55 +0300

    Loading spin function added to the search page.

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -4,6 +4,7 @@ import Footer from "../components/Footer/Footer";
 import ReadMore from "../components/ReadMore/ReadMore";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
 import { Helmet } from "react-helmet-async";
+import { Spin } from "antd";
 
 const useQuery = () => {
   return new URLSearchParams(useLocation().search);
@@ -12,6 +13,7 @@ const useQuery = () => {
 const SearchPage = () => {
   const query = useQuery();
   const searchQuery = query.get("bul");
+  const [loading, setLoading] = useState(false);
   const [results, setResults] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
@@ -22,6 +24,7 @@ const SearchPage = () => {
   useEffect(() => {
     const fetchResults = async () => {
       try {
+        setLoading(true);
         const response = await fetch(
           `${API_URL}/api/search?query=${searchQuery}&page=${currentPage}&limit=${itemsPerPage}`
         );
@@ -36,9 +39,12 @@ const SearchPage = () => {
           setTotalPages(1);
         }
       } catch (error) {
+        setLoading(false);
         console.error("Error fetching search results:", error);
         setResults([]);
         setTotalPages(1);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -55,6 +61,14 @@ const SearchPage = () => {
     }
   };
 
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Spin tip="Loading..." size="large" />
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col md:justify-center md:items-center bg-gray-50 dark:bg-black w-full">
       <Helmet>
@@ -64,91 +78,99 @@ const SearchPage = () => {
         <h1 className="text-3xl text-gray-900 dark:text-white p-5">
           Arama Sonuçları
         </h1>
-        {results.map((result, index) => (
-          <div key={index} className="flex flex-col gap-10 border-t p-5">
-            <div className="flex flex-col md:flex-row gap-5">
-              <div className="md:max-w-[300px]">
-                <Link to={`/${result.postType.toLowerCase()}/${result.slug}`}>
-                  <img
-                    src={result.imgSrc || "/default-image.png"}
-                    alt={result.title}
-                  />
-                </Link>
-              </div>
-              <div>
-                <h1 className="text-gray-900 dark:text-white font-semibold text-xl">
+        {results.length === 0 ? (
+          <p className="text-center text-gray-900 dark:text-white">
+            Aradığınız sonuç bulunamadı.
+          </p>
+        ) : (
+          results.map((result, index) => (
+            <div key={index} className="flex flex-col gap-10 border-t p-5">
+              <div className="flex flex-col md:flex-row gap-5">
+                <div className="md:max-w-[300px]">
                   <Link to={`/${result.postType.toLowerCase()}/${result.slug}`}>
-                    {result.title}
+                    <img
+                      src={result.imgSrc || "/default-image.png"}
+                      alt={result.title}
+                    />
                   </Link>
-                </h1>
-                <p className="text-gray-900 dark:text-white mt-5">
-                  <ReadMore
-                    text={result.text}
-                    limit={100}
-                    to={`/${result.postType.toLowerCase()}/${result.slug}`}
-                  />
-                </p>
+                </div>
+                <div>
+                  <h1 className="text-gray-900 dark:text-white font-semibold text-xl">
+                    <Link to={`/${result.postType.toLowerCase()}/${result.slug}`}>
+                      {result.title}
+                    </Link>
+                  </h1>
+                  <p className="text-gray-900 dark:text-white mt-5">
+                    <ReadMore
+                      text={result.text}
+                      limit={100}
+                      to={`/${result.postType.toLowerCase()}/${result.slug}`}
+                    />
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
-      <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-        <div className="flex flex-1 justify-between sm:hidden">
-          <button
-            onClick={() => handlePageChange("previous")}
-            className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-            disabled={currentPage === 1}
-          >
-            Previous
-          </button>
-          <button
-            onClick={() => handlePageChange("next")}
-            className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-            disabled={currentPage === totalPages}
-          >
-            Next
-          </button>
-        </div>
-        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-          <div>
-            <nav
-              className="isolate inline-flex -space-x-px rounded-md shadow-sm"
-              aria-label="Pagination"
+      {results.length > 0 && (
+        <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+          <div className="flex flex-1 justify-between sm:hidden">
+            <button
+              onClick={() => handlePageChange("previous")}
+              className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+              disabled={currentPage === 1}
             >
-              <button
-                onClick={() => handlePageChange("previous")}
-                className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                disabled={currentPage === 1}
+              Previous
+            </button>
+            <button
+              onClick={() => handlePageChange("next")}
+              className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+              disabled={currentPage === totalPages}
+            >
+              Next
+            </button>
+          </div>
+          <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+            <div>
+              <nav
+                className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+                aria-label="Pagination"
               >
-                <span className="sr-only">Previous</span>
-                <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-              </button>
-              {[...Array(totalPages)].map((_, index) => (
                 <button
-                  key={index}
-                  onClick={() => setCurrentPage(index + 1)}
-                  className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${
-                    index + 1 === currentPage
-                      ? "bg-indigo-600 text-white"
-                      : "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                  } focus:z-20 focus:outline-offset-0`}
+                  onClick={() => handlePageChange("previous")}
+                  className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  disabled={currentPage === 1}
                 >
-                  {index + 1}
+                  <span className="sr-only">Previous</span>
+                  <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
                 </button>
-              ))}
-              <button
-                onClick={() => handlePageChange("next")}
-                className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                disabled={currentPage === totalPages}
-              >
-                <span className="sr-only">Next</span>
-                <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-              </button>
-            </nav>
+                {[...Array(totalPages)].map((_, index) => (
+                  <button
+                    key={index}
+                    onClick={() => setCurrentPage(index + 1)}
+                    className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${
+                      index + 1 === currentPage
+                        ? "bg-indigo-600 text-white"
+                        : "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                    } focus:z-20 focus:outline-offset-0`}
+                  >
+                    {index + 1}
+                  </button>
+                ))}
+                <button
+                  onClick={() => handlePageChange("next")}
+                  className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  disabled={currentPage === totalPages}
+                >
+                  <span className="sr-only">Next</span>
+                  <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+                </button>
+              </nav>
+            </div>
           </div>
         </div>
-      </div>
+      )}
       <Footer />
     </div>
   );

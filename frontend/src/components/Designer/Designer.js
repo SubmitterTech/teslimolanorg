f4bcbac487e98da11cdab668699641a2456d0dca
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-06-30 17:11:06 +0300

    Designer component fixed on bottom on mobile screen mode

--- frontend/src/components/Designer/Designer.js
+++ frontend/src/components/Designer/Designer.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const Designer = () => {
   return (
-    <div className='flex items-start justify-center  w-full bg-gray-700 p-3 text-white text-sm'>
-        <p>© 2024 Designed By SubmitterTech</p>
-    </div>
-  )
+    <>
+      <div className='fixed bottom-0 left-0 w-full bg-gray-700 p-3 text-white text-sm text-center md:hidden'>
+          <p>© 2024 Designed By SubmitterTech</p>
+      </div>
+      <div className='hidden md:block w-full bg-gray-700 p-3 text-white text-sm text-center'>
+          <p>© 2024 Designed By SubmitterTech</p>
+      </div>
+    </>
+  );
 }
 
-export default Designer
+export default Designer;

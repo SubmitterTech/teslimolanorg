9c0b40db17e4451b1d1a05e7d3cce67340f07b9d
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-07-05 21:52:32 +0300

    cors setting updated

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,23 @@ const connectDB = require('./config/db');
 const app = express();
 const port = 5001; //Server Port
 
-app.use(cors());
+// CORS yapılandırması
+const allowedOrigins = [
+  'http://localhost:3000', // Yerel geliştirme ortamı
+  'https://teslimolanorg-frontend.onrender.com/' // Üretim ortamı domaini
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    // origin'in allowedOrigins listesinde olup olmadığını kontrol edin
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 

517ac70332a1ad599f157cfa63ac226858a7523b
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-07-09 14:14:00 +0300

    Added ip address 0.0.0.0.0 so that index.js can be accessed from anywhere

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
+const connectDB = require('./config/db');
+
+// Rotalar
 const adminRoute = require('./routes/adminRoute');
 const configRoute = require('./routes/configRoute');
 const userRoute = require('./routes/userRoute');
 const emailRoute = require('./routes/emailRoute');
 const postsRoute = require('./routes/postsRoute');
 const hutbeRoute = require('./routes/hutbeRoute');
-const path = require('path'); // path modülünü import edin
-const connectDB = require('./config/db');
 
 const app = express();
-const port = 5001; //Server Port
+const port = process.env.PORT || 5001;  // Tavsiye edilen: PORT'u .env dosyasından alın
 
 // CORS yapılandırması
 const allowedOrigins = [
@@ -24,11 +26,9 @@ const allowedOrigins = [
   'http://157.230.18.195:5001' // Droplet üzerindeki backend
 ];
 
-
 app.use(cors({
   origin: (origin, callback) => {
-    // origin'in allowedOrigins listesinde olup olmadığını kontrol edin
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
@@ -38,13 +38,14 @@ app.use(cors({
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// Public klasöründeki statik dosyalara erişim sağlar
-/* app.use('/uploads', express.static('public/uploads')); */
+
 // Statik dosyaların servis edilmesi
 app.use('/uploads', express.static(path.join(__dirname, 'assets/uploads')));
 
+// Veritabanına bağlan
 connectDB();
 
+// API Rotaları
 app.use('/api/admin', adminRoute);
 app.use('/api', configRoute);
 app.use('/api/admin', userRoute);
@@ -52,6 +53,7 @@ app.use('/api', postsRoute);
 app.use('/api', emailRoute);
 app.use("/api/hutbeler", hutbeRoute);
 
-app.listen(port, () => {
+// Sunucuyu tüm ağ arayüzlerinde dinlemek
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });

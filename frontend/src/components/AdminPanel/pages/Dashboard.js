fc53d512d057abd479d392e8310cc8aa4110b03c
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-07-11 13:15:14 +0300

    dashboard updated

--- frontend/src/components/AdminPanel/pages/Dashboard.js
+++ frontend/src/components/AdminPanel/pages/Dashboard.js
@@ -4,7 +4,8 @@ const Dashboard = () => {
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4">Yönetici Paneli</h2>
-      <p>Welcome to the admin dashboard!</p>
+      <p>Bismillahirrahmanirrahim</p>
+      <p>En Lütufkar ve En Merhametli olan TANRI'nın adıyla</p>
     </div>
   );
 };
